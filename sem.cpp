7b9ea2564ffa23a9c6ea58756709066c57f88868
Simuhin Egor <simuhinegor22@gmail.com>
2024-02-20 16:50:11 +0300

    Start

--- sem.cpp
+++ sem.cpp
@@ -1,18 +1,1 @@
-#include <iostream>
-using namespace std;
-unsigned long long int factorial(unsigned int s)
-{
-    unsigned long int m = 1;
-    for(int i = 1;i <= s;i++)
-    {
-        m = i * m;
-    }
-    return m;
-}
-int main ()
-{
-    unsigned  int b;
-    cin >> b;
-        cout << factorial(b) << endl;
-    return 0;
-}
+
---
+++ space.cpp
@@ -0,0 +1,94 @@
+class Galaxys_all(){
+	Galaxys_all(){
+	
+	}
+
+};
+
+class Milky_Way(): public Galaxys_all{
+
+protected:
+
+public:
+
+	string Name_of_Planets(int a){
+		switch(a) {
+		case'1':
+			return "Mercury";
+			break;
+		case'2':
+			return "Venus";
+			break;
+		case'3':
+			return "Earth";
+			break;
+		case'4':
+			return "Mars";
+			break;
+		case'5':
+			return "Your planet is Jupiter";
+			break;
+		case'6':
+			return "Saturn";
+			break;
+		case'7':
+			return "Uranus";
+			break;
+		case'8':
+			return "Neptune";
+		default:
+			return "There is no planet with this numder";	
+		}
+	}	
+};
+
+class Mercury:public Milky_Way{
+private:
+
+	bool Live;
+	float Me_Radius;
+	float Massa;
+	float Voluem;
+	bool atmoshere;
+	bool 
+	
+public:	
+};
+class Venus:public Milky_Way{
+
+}; 
+
+
+class Earth:public Milky_Way{
+private:
+	
+public:
+
+
+};
+
+class Mars:public Milky_Way{
+protrected:
+
+
+public:
+
+
+};
+
+class LIVE:public "Name every planets"{
+
+
+}; //every planets
+
+	   
+	   
+class Animal:public LIVE {
+
+
+
+};
+
+
+
+class Rasteni:
